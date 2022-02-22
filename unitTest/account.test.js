bab7b9c550b02a5750a41f954b6b11bec74f893b
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-22 14:51:54 +0000

    tdd empty transactionList

--- unitTest/account.test.js
+++ unitTest/account.test.js
@@ -14,12 +14,17 @@ describe("Account Class", () => {
     account.withdraw(50);
     expect(account.balance).toEqual(50);
   });
-  it('should throw an error when withdrawing if balance is 0', () => {
-      account.balance = 0
-      expect(() => account.withdraw(100)).toThrow()
-  }) 
-  it('should throw an error when withdrawing if balance is less than withdraw amount', () => {
-    account.balance = 75
-    expect(() => account.withdraw(100)).toThrow()
-}) 
+  it("should throw an error when withdrawing if balance is 0", () => {
+    account.balance = 0;
+    expect(() => account.withdraw(100)).toThrow();
+  });
+  it("should throw an error when withdrawing if balance is less than withdraw amount", () => {
+    account.balance = 75;
+    expect(() => account.withdraw(100)).toThrow();
+  });
+
+  it('should start with empty transaction list', () => {
+      account1 = new Account()
+      expect(account1.transactionList.length).toEqual(0)
+  })
 });
