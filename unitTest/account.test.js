832a39cf50da5aa4da5789a66ec4d3cbf056dc72
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-22 10:39:56 +0000

    tdd user story 2

--- unitTest/account.test.js
+++ unitTest/account.test.js
@@ -1,7 +1,12 @@
-const Account = require('../account')
-describe('Account Class', () => {
-    it('Should create an account with 0 balance', () => {
-        const account = new Account()
-        expect(account.balance).toEqual(0)
-    })
-})
+const Account = require("../account");
+describe("Account Class", () => {
+  const account = new Account();
+  it("Should create an account with 0 balance", () => {
+    expect(account.balance).toEqual(0);
+  });
+
+  it("balance should  increase by deposit amount", () => {
+      account.deposit(100)
+      expect(account.balance).toEqual(100)
+  })
+});
