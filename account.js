b66a02ef04812d718107af4e7fbd150be072a740
robin ucar <mehmet.ucar@imediapro.co.uk>
2022-02-22 14:40:36 +0000

    user story 4 completed , test passed, conditional error added to the withdraw method

--- account.js
+++ account.js
@@ -7,6 +7,9 @@ class Account {
     }
 
     withdraw(withdraw_amount) {
+        if(this.balance == 0) {
+            throw new Error('Payment error: You do not have enough balance for this transaction')
+        }
         return this.balance -= withdraw_amount
     }
 
